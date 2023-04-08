6fe3309d0b4001a28d2a6c0f7e620c32bcb87c64
RajVamja <vamjaraj40@gmail.com>
2023-04-08 23:17:48 +0530

    First commit

---
+++ Practice.sql
@@ -0,0 +1,36 @@
+create database DBRajVamja 
+use DBRajVamja; 
+drop table Products
+create table Products(
+	ProductID int,
+	Name varchar(100),
+	ProductNumber nvarchar(50)
+);
+select * from Products
+select * from Production.Product
+use DBRajVamja;
+use AdventureWorks2019;
+drop table ProductDetails
+select * from Pro
+
+select * from ProductDetails
+create table ProductDetails(
+	ProductID int,
+	Color nvarchar(30),
+	SafetyStockLevel smallint,
+	ReorderPoint smallint,
+	StandardCost money,
+	ListPrice money,
+	Size nvarchar(20)
+);
+select * from [Purchasing].[PurchaseOrderHeader]
+select * from [Purchasing].[PurchaseOrderDetail]
+select * from [HumanResources].[Employee]
+
+
+
+
+
+
+
+
