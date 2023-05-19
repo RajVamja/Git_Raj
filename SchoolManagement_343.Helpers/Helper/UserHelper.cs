b1a6ee109e07cff80bf23a3adcfb783027a0bb11
Raj_Vamja <113017462+RajVamja@users.noreply.github.com>
2023-05-19 10:08:17 +0530

    Add files via upload

---
+++ SchoolManagement_343.Helpers/Helper/UserHelper.cs
@@ -0,0 +1,24 @@
+﻿using SchoolManagement_343.Models.Context;
+using SchoolManagement_343.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement_343.Helpers.Helper
+{
+    public class UserHelper
+    {
+        public static User convert(CustomUserModel cu)
+        {
+            User u = new User();
+            u.uId = cu.uId;
+            u.fName = cu.fName;
+            u.lName = cu.lName;
+            u.userName = cu.userName;
+            u.passWord = cu.passWord;
+            return u;
+        }
+    }
+}
