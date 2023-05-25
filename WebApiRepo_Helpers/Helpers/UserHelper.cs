b96ac356ea7aba6bafdbeb2133a5c82f3587b518
Raj_Vamja <113017462+RajVamja@users.noreply.github.com>
2023-05-25 18:11:51 +0530

    Add files via upload

---
+++ WebApiRepo_Helpers/Helpers/UserHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiRepo_Models.Context;
+using WebApiRepo_Models.Model;
+
+namespace WebApiRepo_Helpers.Helpers
+{
+    public class UserHelper
+    {
+        public static User convert(CustomUserModel cu)
+        {
+            User u = new User();
+            u.uId = cu.uId;
+            u.fName = cu.fName;
+            u.lName = cu.lName;
+            u.userName = cu.userName;
+            u.passWord = cu.passWord;
+            return u;
+        }
+    }
+}
